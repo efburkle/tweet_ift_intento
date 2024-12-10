cad3f9c4ad483344626f713ff3c81a0a39246bbd
efburkle <0258701@up.edu.mx>
2024-12-09 20:27:52 -0600

    Update pipeline.py

--- src/pipelines/pipeline.py
+++ src/pipelines/pipeline.py
@@ -10,11 +10,19 @@ PIPELINE_ROOT = "gs://tweet_ift_intento/pipeline_root"
 
 @kfp.dsl.pipeline(name=PIPELIE_NAME, pipeline_root=PIPELINE_ROOT)
 def pipeline(project_id: str, location: str, bq_dataset: str, bq_table: str):
-    from tweet_ift_intento.code.libs import train_func.py
+#    from tweet_ift_intento.code.libs import train_func
     from components.evaluation import choose_best_model
-    from components.models import decision_tree, random_forest
+#    from components.models import decision_tree, random_forest
     from components.register import upload_model
 
+    if os.path.isdir(os.path.abspath(os.path.join("tweet_ift_intento","code", "libs"))):
+        sys.path.append(os.path.abspath(os.path.join("tweet_ift_intento","code", "libs")))
+        from train_func import *
+    else:
+        raise ModuleNotFoundError(
+            "The 'libs' directory does not exist in the specified path."
+        )
+    
     data_op = load_data(
         project_id=project_id, bq_dataset=bq_dataset, bq_table=bq_table
     ).set_display_name("Load data from BigQuery")
