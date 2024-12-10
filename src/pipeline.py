9733b47b364644843dc011bcc71931b32861d40f
efburkle <0258701@up.edu.mx>
2024-12-09 22:14:38 -0600

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -17,8 +17,8 @@ def pipeline(project_id: str, location: str, bq_dataset: str, bq_table: str):
     from code.train_models import train_ift, train_sentiment  # Funciones para entrenar los modelos de clasificación
 
     
-    if os.path.isdir(os.path.abspath(os.path.join("tweet_ift_intento","code", "train_models"))):
-        sys.path.append(os.path.abspath(os.path.join("tweet_ift_intento","code", "train_models")))
+    if os.path.isdir(os.path.abspath(os.path.join("code", "train_models"))):
+        sys.path.append(os.path.abspath(os.path.join("code", "train_models")))
         from train_ift import read_sentiment_data, read_ift_data, clean_tweets, equal_sample, tokenize_tweets, train_test_split_tweets, train_vectorizer, vectorize_tweets, train_model, eval_model
         from train_sentiment import read_sentiment_data, clean_tweets, tokenize_tweets, train_test_split_tweets, train_vectorizer, vectorize_tweets, train_model, eval_model
     else:
