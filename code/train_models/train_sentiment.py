859ee86efcc69feae868611fde47f523cb29a8b4
LuisGonzalez98 <159965886+LuisGonzalez98@users.noreply.github.com>
2024-12-10 10:52:27 -0600

    Update train_sentiment.py

--- code/train_models/train_sentiment.py
+++ code/train_models/train_sentiment.py
@@ -11,7 +11,7 @@ warnings.filterwarnings("ignore")
 # Todas las funciones usadas en este código se encuentran en train_func.py en la carpeta libs
 if os.path.isdir(os.path.abspath(os.path.join("code", "libs"))):
     sys.path.append(os.path.abspath(os.path.join("code", "libs")))
-      from ..libs.train_func import *
+    from ..libs.train_func import *
 else:
     raise ModuleNotFoundError(
         "The 'libs' directory does not exist in the specified path."
