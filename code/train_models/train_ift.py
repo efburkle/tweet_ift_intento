dfccff37c417e640901e8d193cc8130174d16d78
LuisGonzalez98 <159965886+LuisGonzalez98@users.noreply.github.com>
2024-12-10 21:08:53 -0600

    Update train_ift.py

--- code/train_models/train_ift.py
+++ code/train_models/train_ift.py
@@ -28,24 +28,24 @@ model = LogisticRegression()
 #tweets_ift = read_ift_data(project_id, dataset_id, "train")
 
 # Limpiamos los datos (código de limpieza en la carpeta libs/train_func.py)
-tweets = clean_tweets(tweets)
-tweets_ift = clean_tweets(tweets_ift, sample=False)
+#tweets = clean_tweets(tweets)
+#tweets_ift = clean_tweets(tweets_ift, sample=False)
 # Igualamos muestras y las juntamos
-tweets = equal_sample(tweets, tweets_ift)
+#tweets = equal_sample(tweets, tweets_ift)
 # Tokenizamos los tweets
-tweets = tokenize_tweets(tweets)
+#tweets = tokenize_tweets(tweets)
 # Dividimos datos en train y test
-X_train, X_test, y_train, y_test = train_test_split_tweets(tweets, y=TELECOM)
+#X_train, X_test, y_train, y_test = train_test_split_tweets(tweets, y=TELECOM)
 
 # Entrenamos vectorizador, se guarda en la carpeta models
-vectorizer = train_vectorizer(X_train, PATH=MODELS + SLASH + IFT_VECTORIZER)
+#vectorizer = train_vectorizer(X_train, PATH=MODELS + SLASH + IFT_VECTORIZER)
 
 # Vectorizamos tweets
-X_train = vectorize_tweets(vectorizer, X_train)
-X_test = vectorize_tweets(vectorizer, X_test)
+#X_train = vectorize_tweets(vectorizer, X_train)
+#X_test = vectorize_tweets(vectorizer, X_test)
 
 # Entrenamos
-model = train_model(X_train, y_train, param_lr, model, PATH=MODELS + SLASH + IFT_MODEL)
+#model = train_model(X_train, y_train, param_lr, model, PATH=MODELS + SLASH + IFT_MODEL)
 
 # Evaluamos modelo
-eval_model(model, X_train, y_train, X_test, y_test)
+#eval_model(model, X_train, y_train, X_test, y_test)
